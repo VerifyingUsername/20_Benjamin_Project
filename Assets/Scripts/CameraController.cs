15e8765022d5a8e94278199031bb0d4322fdb0c0
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2021-02-03 16:52:11 +0800

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject playerGO;
+    Vector3 offset = new Vector3(0, 3f, 4f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, playerGO.transform.position + offset, 0.1f);
+    }
+}
